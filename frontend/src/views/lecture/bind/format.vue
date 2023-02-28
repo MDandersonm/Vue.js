b1870786b1443ad2294e46fd37e800676631248b
MDandersonm <1215kimyj@gmail.com>
2023-02-28 17:11:33 +0900

    KH수업  뷰js

---
+++ frontend/src/views/lecture/bind/format.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        <div>
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
